e14e6c73167746cf01eabacdebfaec3ba984f5f1
Evan Shimizu <ebshimizu@gmail.com>
2021-01-17 16:40:37 -0500

    starting to port caster frame to vue for mvp
    (it's so nice)

--- static/srv/js/caster-frame.js
+++ static/srv/js/caster-frame.js
@@ -1,81 +1,113 @@
+const socket = io('http://localhost:3005/');
 
-const socket = io('http://localhost:3005/')
+const app = new Vue({
+  el: '#app',
+  beforeCreate() {
+    socket.on('requestID', () => {
+      socket.emit('reportID', this.id);
+    });
+
+    socket.on('update', (state) => {
+      this.state = state;
+    });
+    socket.on('changeTheme', (themeDir) => {
+      changeTheme(themeDir, 'caster-frame.css');
+    });
+  },
+  data() {
+    return {
+      id: 'Caster Frame',
+      state: {},
+      test: 'test',
+    };
+  },
+  computed: {
+    casterOneName() {
+      return this.getCaster(0)?.name
+    },
+    casterOneSocial() {
+      return this.getCaster(0)?.social
+    },
+    casterOneTextSize() {
+      return this.getCaster(0)?.textSize
+    }
+  },
+  methods: {
+    getCaster(index) {
+      if (this.state.casters) {
+        return this.state.casters.length > index ? this.state.casters[index] : {}
+      }
+    }
+  }
+});
 
 class CasterFrame {
-  constructor (variant) {
-    this.name = `Caster Frame - ${variant}`
+  constructor(variant) {
+    this.name = `Caster Frame - ${variant}`;
   }
 
-  ID () {
+  ID() {
     return {
-      name: this.name
-    }
+      name: this.name,
+    };
   }
 
   // changes up the state n stuff
-  updateState (state) {
+  updateState(state) {
     if (state.casters.one) {
-      $('#caster-1-name').text(state.casters.one.name)
-      $('#caster-1-social').text(state.casters.one.social)
-      $('#caster-1-name').removeClass().addClass(state.casters.one.size)
-      $('#caster-1-social').removeClass().addClass(state.casters.one.size)
+      $('#caster-1-name').text(state.casters.one.name);
+      $('#caster-1-social').text(state.casters.one.social);
+      $('#caster-1-name').removeClass().addClass(state.casters.one.size);
+      $('#caster-1-social').removeClass().addClass(state.casters.one.size);
     }
 
     if (state.casters.two) {
-      $('#caster-2-name').text(state.casters.two.name)
-      $('#caster-2-social').text(state.casters.two.social)
-      $('#caster-2-name').removeClass().addClass(state.casters.two.size)
-      $('#caster-2-social').removeClass().addClass(state.casters.two.size)
+      $('#caster-2-name').text(state.casters.two.name);
+      $('#caster-2-social').text(state.casters.two.social);
+      $('#caster-2-name').removeClass().addClass(state.casters.two.size);
+      $('#caster-2-social').removeClass().addClass(state.casters.two.size);
     }
 
     if (state.casters.tournament) {
-      $('#tournament-name').text(state.casters.tournament)
+      $('#tournament-name').text(state.casters.tournament);
     }
 
     // dynamic frame
     if ($('body.dynamic').length > 0) {
-      const id = state.casters.count === 1 ? 'single-caster-frame' : 'duo-caster-frame'
-      $('body.dynamic').attr('id', id)
+      const id =
+        state.casters.count === 1 ? 'single-caster-frame' : 'duo-caster-frame';
+      $('body.dynamic').attr('id', id);
     }
 
     // sidebar elems (not present in all, so pretty low overhead)
     // create elems
-    $('#sidebar-title').text(state.casters.sidebar.title)
-    $('.sidebar-list').html('')
+    $('#sidebar-title').text(state.casters.sidebar.title);
+    $('.sidebar-list').html('');
 
     if (state.casters.sidebar.text) {
-      const lines = state.casters.sidebar.text.split('\n')
+      const lines = state.casters.sidebar.text.split('\n');
       for (let i = 0; i < lines.length; i++) {
         $('.sidebar-list').append(`
           <div class="list-item">
             <div class="list-text">${lines[i]}</div>
           </div>
-        `)
+        `);
       }
     }
 
-    $('#blue-team-name').text(state.blueTeam.name)
-    $('#red-team-name').text(state.redTeam.name)
-    $('#blue-team-score').text(isNaN(state.blueTeam.score) ? 0 : state.blueTeam.score)
-    $('#red-team-score').text(isNaN(state.redTeam.score) ? 0 : state.redTeam.score)
+    $('#blue-team-name').text(state.blueTeam.name);
+    $('#red-team-name').text(state.redTeam.name);
+    $('#blue-team-score').text(
+      isNaN(state.blueTeam.score) ? 0 : state.blueTeam.score
+    );
+    $('#red-team-score').text(
+      isNaN(state.redTeam.score) ? 0 : state.redTeam.score
+    );
 
-    setCSSImage('#blue-team-logo', state.blueTeam.logo)
-    setCSSImage('#red-team-logo', state.redTeam.logo)
+    setCSSImage('#blue-team-logo', state.blueTeam.logo);
+    setCSSImage('#red-team-logo', state.redTeam.logo);
 
     // variant options
-    $('body').attr('frame-variant', state.casters.frame)
+    $('body').attr('frame-variant', state.casters.frame);
   }
 }
-
-function initCasterFrame (variantName) {
-  // just kinda runs on page load huh
-  const casterFrame = new CasterFrame(variantName)
-
-  socket.on('requestID', () => {
-    socket.emit('reportID', casterFrame.ID())
-    socket.emit('requestState')
-  })
-
-  socket.on('update', (state) => { casterFrame.updateState.call(casterFrame, state) })
-  socket.on('changeTheme', (themeDir) => { changeTheme(themeDir, 'caster-frame.css') })
-};
