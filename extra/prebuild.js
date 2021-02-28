9ddf4e7817a34f1ad9caa781bf2a215795cc65ed
Evan Shimizu <ebshimizu@gmail.com>
2021-02-28 17:23:23 -0500

    fix the prebuild script since i moved things

--- extra/prebuild.js
+++ extra/prebuild.js
@@ -1,4 +1,4 @@
 const fs = require('fs-extra')
 
-fs.copyFileSync('./static/srv/js/vue@2.prod.js', './static/srv/js/vue@s.js')
+fs.copyFileSync('./static/srv/js/lib/vue@2.prod.js', './static/srv/js/lib/vue@s.js')
 console.log('set overlay vue dist to prod')
